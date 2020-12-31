05806907eceb015782d3692cbdd5a456e391beea
D.O. Domela Nieuwenhuis Nyegaard <s1853767@huisuil03.vuw.leidenuniv.nl>
2020-12-31 18:29:54 +0100

    more insight testing

--- saved_models/score.py
+++ saved_models/score.py
@@ -1,21 +1,23 @@
 import pickle
-from utilities.data_loader import prepare_dataset
-from dataset.data_loader import SemEvalDataLoader
-from sklearn.metrics import accuracy_score, f1_score, recall_score
+import os
+#from utilities.data_loader import get_embeddings, Task4Loader, prepare_dataset
+#from dataset.data_loader import SemEvalDataLoader
+#from sklearn.metrics import accuracy_score, f1_score, recall_score
 
-def evaluation_report(correct, predicted):
-	return recall_score(correct, predicted, average="macro"), #avgRecall
-	(f1_score(correct, predicted, labels=['positive'])+f1_score(correct, predicted, labels=['negative']))/2,#f1posneg
-	accuracy_score(correct, predicted)#accuracy
+#def evaluation_report(correct, predicted):
+#	return recall_score(correct, predicted, average="macro"), #avgRecall
+#	(f1_score(correct, predicted, labels=['positive'])+f1_score(correct, predicted, labels=['negative']))/2,#f1posneg
+#	accuracy_score(correct, predicted)#accuracy
 
 if __name__ == '__main__':
-	embeddings, word_indices = get_embeddings(corpus="datastories.twitter", dim=300)
-	gold_data = SemEvalDataLoader().get_gold(task=TASK)
-	gX = [obs[1] for obs in gold_data]
-	gy = [obs[0] for obs in gold_data]
-	gold = prepare_dataset(gX, gy, loader.pipeline, loader.y_one_hot)
-	print(gold)
-
-	loaded_model = pickle.load(open('baseline.pickle', 'rb'))
-	predicted = loaded_model.predict(gX)
-	print(evaluation_report(gY, predicted))
+	models = os.listdir("./saved_models")
+	models.remove("score.py") 
+	results_dict = {}
+	for model in models:
+		print("_____MODEL_"+model+"_____")
+		results_dict[os.path.splitext(model)[0]] = pickle.load(open('saved_models/'+model, 'rb'))
+		print(results_dict[os.path.splitext(model)[0]].keys())
+		print(results_dict[os.path.splitext(model)[0]]['3-test.f1_pn'])
+		print(results_dict[os.path.splitext(model)[0]]['3-test.M_recall'])
+		print(results_dict[os.path.splitext(model)[0]]['3-test.M_precision'])
+		input()
